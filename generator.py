c215e85cffec0bc926697103838e883ea67717c7
Xuanyu Wang <xuanyu.wang@outlook.com>
2017-04-07 10:44:59 -0700

    add test cases

--- generator.py
+++
@@ -1,6 +0,0 @@
-unitSize = 1024 * 16
-ext = ".txt"
-for i in range(5):
-    with open(str(i) + ext, 'w') as file:
-        file.write("********************************\n")
-        file.write(str(i) * (2 ** i) * unitSize)
